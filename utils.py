55c94f799ae550ce5b7b9a95db930b5b2016d923
Art_K <artemkavalerov65@gmail.com>
2022-10-01 03:33:37 +0300

    внесены изменения для решения ДЗ23 по функциональному программированию дополнения до 24 ДЗ

--- utils.py
+++ utils.py
@@ -1,15 +1,20 @@
-def solution_query(cmd, value, file):
+import re
+from typing import Iterator, List, Any
+
+
+def solution_query(cmd: str, value: str, file: Iterator) -> List[Any]:
     if cmd == 'filter':
-        return filter(lambda x: value in x, file)
+        return list(filter(lambda x: value in x, file))
+    elif cmd == 'regex':
+        regex = re.compile(r'(value)')
+        return list(filter(lambda x: regex.findall(x), file))
     elif cmd == 'map':
         value = int(value)
-        return map(lambda x: x.split(" ")[value], file)
+        return list(map(lambda x: x.split(" ")[value], file))
     elif cmd == 'unique':
         return list(set(file))
     elif cmd == 'sort':
-        if value == "desc":
-            return sorted(file, reverse=True)
-        return sorted(file)
+        return sorted(file, reverse=(value == "desc"))
     elif cmd == 'limit':
         value = int(value)
         return list(file)[:value]
